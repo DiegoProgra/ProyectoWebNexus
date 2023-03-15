06daf916105550a641efa704889957f2c9ae971b
Diego <Diego@DESKTOP-HCUPR7E>
2023-03-15 13:00:31 -0600

    PrimerAvanceOficial

---
+++ src/main/java/NexusApp/NexusAppApplication.java
@@ -0,0 +1,13 @@
+package NexusApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusAppApplication.class, args);
+	}
+
+}
